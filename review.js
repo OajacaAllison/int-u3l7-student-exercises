feea17272a5c5b07d83437448952af9a41b62708
OajacaAllison <oajacaallison@gmail.com>
2025-06-11 15:35:20 -0400

    meow

--- review.js
+++ review.js
@@ -3,23 +3,26 @@
 
 // 2. Take look at the HMTL. When the user clicks the "option-one" button, we want the "option-one-screen" div to show. 
 //  - First, store those elements as variables. Can someone take me through the steps to do that?
-
+let optionOneScreen= document.querySelector('.option-one-screen')
 
 // CODE SOLO, PART 1
 // 3. Declare a variable to save the button with the class "option-two"
 // 4. Declare a variable to save the div with the class "option-two-screen"
 
+let optionTwoScreen= document.querySelector('.option-two-screen')
 
 
 // CODE ALONG, PART 2
 // 5. Let's use the console to check our variables saved correctly. 
 //  - Watch as I use console.log() to check the contents of our option-one variables.
-
+console.log(optionOneScreen)
+console,log(optionTwoScreen)
 
 // CODE SOLO, PART 2
 // 6. Now use console.log() to check the contents of the option-two variables you created. 
 //  - Did they save correctly? If not, fix them!
 
+optionOneBtn.log(optionOneButton)
 
 
 
